5e0caca5a39885736997423a709e9dd1a20f35f1
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2020-12-15 12:27:59 +0800

    training: add index on controller and views

--- app/Http/Controllers/TrainingController.php
+++ app/Http/Controllers/TrainingController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class TrainingController extends Controller
 {
-    //
+    public function index()
+    {
+        // query trainings from trainings table using model
+        $trainings = \App\Models\Training::all();
+
+        // dd($trainings); // dump and die
+
+        // return to view with $trainings
+        // resources/views/trainings/index.blade.php
+        return view('trainings.index', compact('trainings'));
+    }
 }
---
+++ resources/views/trainings/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Training Index') }}</div>
+
+                <div class="card-body">
+                    <table class="table table-hover table-responsive">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Title</th>
+                                <th>Description</th>
+                                <th>Created At</th>
+                                <th>Created DateTime</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($trainings as $training)
+                                <tr>
+                                    <td>{{ $training->id }}</td>
+                                    <td>{{ $training->title }}</td>
+                                    <td>{{ $training->description }}</td>
+                                    <td>{{ $training->created_at ? $training->created_at->diffForHumans() : 'NO DATA' }}</td>
+                                    <td>{{ $training->created_at ?? '-' }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
