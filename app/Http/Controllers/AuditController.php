4cc00c776296e91e79e26639f13c14f6a58c55af
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2020-12-29 14:34:06 +0800

    Day 6: Displaying Audit Trails

---
+++ app/Http/Controllers/AuditController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use OwenIt\Auditing\Models\Audit;
+
+class AuditController extends Controller
+{
+    public function audit()
+    {
+        // query all audits
+        $audits = Audit::orderBy('created_at', 'desc')->get();
+
+        // return to view
+        return view('audit', compact('audits'));
+    }
+}
